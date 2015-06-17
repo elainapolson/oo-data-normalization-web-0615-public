36f17f97b51d9f10fd751cfa26ca82176693b94a
elainapolson <elainapolson@gmail.com>
2015-06-17 14:56:31 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,9 @@
 class Song
-  # code goes here
+  attr_accessor :title, :artist
+
+  def serialize
+    text_file = self.title.gsub(" ", "_").downcase
+    File.open("./tmp/#{text_file}.txt", 'w') { |f| f.write "#{@artist.name} - #{@title}"}
+  end
+
 end
